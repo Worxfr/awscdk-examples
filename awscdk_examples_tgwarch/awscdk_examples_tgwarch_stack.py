ffc074f1a7302067c39b117b8aca490cac7839e5
Worxfr <worx@pouf.org>
2024-05-16 14:44:00 +0200

    review file organisation

--- awscdk_examples_tgwarch/awscdk_examples_tgwarch_stack.py
+++ awscdk_examples_tgwarch/awscdk_examples_tgwarch_stack.py
@@ -5,15 +5,56 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class AwscdkExamplesTgwarchStack(Stack):
+class awscdk_examples_tgwarch_stack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, "MyVpc",
+                      max_azs=1,
+                      ip_addresses=ec2.IpAddresses.cidr("10.10.0.0/16"),
+                      subnet_configuration=[ec2.SubnetConfiguration(
+                          subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                          name="vpc1sub1Routable",
+                          cidr_mask=24
+                      )]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwscdkExamplesTgwarchQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc1cidr2 = ec2.CfnVPCCidrBlock(self, "VPC1CIDR2",
+            vpc_id=vpc.vpc_id,
+            cidr_block="100.64.0.0/16"
+        )
+
+        private_subnet = ec2.CfnSubnet(self, "vpc1sub2NonRoutable",
+            availability_zone=vpc.availability_zones[0],
+            cidr_block="100.64.0.0/24",
+            vpc_id=vpc.vpc_id,
+            tags= [cdk.CfnTag(key="Name", value="Tgwarch/MyVpc/vpc1sub2NonRoutable")]) 
+
+
+        private_subnet.add_dependency(vpc1cidr2)
+        
+
+        vpc2 = ec2.Vpc(self, "MyVpc2",
+                max_azs=1,
+                ip_addresses=ec2.IpAddresses.cidr("10.20.0.0/16"),
+                subnet_configuration=[ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                    name="vpc2sub1Routable",
+                    cidr_mask=24
+                )]
+        )
+        vpc2cidr2 = ec2.CfnVPCCidrBlock(self, "VPC2CIDR2",
+            vpc_id=vpc2.vpc_id,
+            cidr_block="100.64.0.0/16"
+        )
+
+        private_subnet2 = ec2.CfnSubnet(self, 
+            "vpc2sub2NonRoutable",
+            availability_zone=vpc2.availability_zones[0],
+            cidr_block="100.64.0.0/24",
+            vpc_id=vpc2.vpc_id,
+            tags= [cdk.CfnTag(key="Name", value="Tgwarch/MyVpc2/vpc2sub2NonRoutable")])
+        
+        private_subnet2.add_dependency(vpc2cidr2)
