b7264c6b9a8115db7653ba338094fd9d1bea4e0f
Worxfr <worx@pouf.org>
2024-05-16 14:49:21 +0200

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
-import awscdk_examples_tgwarch_stack
+import awscdk_examples_tgwarch_stack.awscdk_examples_tgwarch_stack
 from constructs import Construct
 
 
